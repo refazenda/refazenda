3b6ea22e4147341bd920ac9b9a5e1c534ec84348
rafael-ravena <rafael.ravena@gmail.com>
2019-04-16 13:38:27 -0300

    deploy from dev

--- js/script.js
+++ js/script.js
@@ -26,24 +26,59 @@ function showImageTheater(image, alt) {
 function hideImageTheater() {
   $("body").removeClass("showing");
 }
+function validateForm(){
+  $('[data-behavior~="send-contact"]').validate({
+    rules: {
+      nome: "required",
+      email: {
+        required: true,
+        email: true
+      },
+      data: {
+        required: true,
+        date: true
+      },
+      pax: {
+        required: true,
+        number: true
+      }
+    },
+    messages: {
+      nome: "Por favor, nos informe seu nome.",
+      email: {
+        required: "Por favor, nos informe seu e-mail.",
+        email: "Por favor, informe um endereço de e-mail correto"
+      },
+      data: {
+        required: "Por favor, nos informe a data da sua festa",
+        date: "Por favor, escolha uma data válida."
+      },
+      pax: {
+        required: "Por favor, nos informe o número aproximado de pessoas<br/>(e não tem problema se mudar depois)",
+        number: "Por favor, informe somente o número (sem ponto, vírgula ou qualquer sinal)"
+      }
+    },
+    submitHandler: function(form, event) {
+      event.preventDefault();
+      $('[data-behavior~="submit-form"]').attr("disabled", "disabled");
+      $.post($(this).attr("action"), $(this).serialize(), function () {
+        toastr["success"]("Obrigado pelo contato! Entraremos em contato o mais breve possível.", "Contato enviado com sucesso!");
+        $('[data-behavior~="reset-form"]').click();
+        $('[data-behavior~="submit-form"]').removeAttr("disabled");
+      }).fail(function () {
+        toastr["error"]("Não conseguimos processar suas informações.<br>Por favor verifique os campos preenchidos e tente novamente.", "Erro ao enviar contato =(");
+        $('[data-behavior~="submit-form"]').removeAttr("disabled");
+      });
+      }
+  });
+}
 $(document).ready(function () {
   setMenuHeight();
   $(window).on("scroll", function(){
     handleScroll();
   });
   setMenuClick();
-  $('[data-behavior~="send-contact"]').on("submit", function (envent) {
-    event.preventDefault();
-    $('[data-behavior~="submit-form"]').attr("disabled", "disabled");
-    $.post($(this).attr("action"), $(this).serialize(), function () {
-      toastr["success"]("Obrigado pelo contato! Entraremos em contato o mais breve possível.", "Envio com sucesso");
-      $('[data-behavior~="reset-form"]').click();
-      $('[data-behavior~="submit-form"]').removeAttr("disabled");
-    }).fail(function () {
-      toastr["error"]("Algo de errado não está certo! Veja se todos os campos foram devidamente preenchidos e tente nevamente mais tarde, por favor.", "Erro ao enviar");
-      $('[data-behavior~="submit-form"]').removeAttr("disabled");
-    });
-  });
+  validateForm();
   $("[data-behavior~='gallery-item']").on("click", function(){
     showImageTheater($(this).data("src"), $(this).data("alt"));
   });
@@ -54,13 +89,13 @@ toastr.options = {
   "debug": false,
   "newestOnTop": true,
   "progressBar": false,
-  "positionClass": "toast-top-right",
+  "positionClass": "toast-top-center",
   "preventDuplicates": false,
   "onclick": null,
-  "showDuration": "300",
-  "hideDuration": "500",
-  "timeOut": "5000",
-  "extendedTimeOut": "1000",
+  "showDuration": 300,
+  "hideDuration": 500,
+  "timeOut": 325000,
+  "extendedTimeOut": 321000,
   "showEasing": "swing",
   "hideEasing": "linear",
   "showMethod": "fadeIn",
